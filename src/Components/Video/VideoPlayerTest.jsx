ac01e9ecdabde83748e804e39c9034fa31baeea3
hassaan141 <hassaanfarooqi2000@gmail.com>
2024-03-27 12:58:15 -0400

    herer

--- src/Components/Video/VideoPlayerTest.jsx
+++ src/Components/Video/VideoPlayerTest.jsx
@@ -1,73 +1,37 @@
-import React , {useState, useEffect, useRef}from 'react'
-import img from '../Video/nocamera.jpg'
-import './VideoPlayerTest.css'
-// import CameraLong from '../CameraListView/CameraLong';
-import {useData} from '../../send-backend/dataContext'
+import React from 'react';
+import img from '../Video/nocamera.jpg';
+import './VideoPlayerTest.css';
+import { useData } from '../../send-backend/dataContext';
 
 function VideoPlayerTest() {
+  const { videoSelections } = useData();
 
-  const {showVideo, image} = useData();
-
-  // const [showVideo1, setShowVideo1] = useState(false);
-  // const [showVideo2, setShowVideo2] = useState(false);
-  // const [showVideo3, setShowVideo3] = useState(false);
-  // const [showVideo4, setShowVideo4] = useState(false);
-  // const [image, setImage] = useState(true);
-
-  const videoRefs = useRef([
-    React.createRef(),
-    React.createRef(),
-    React.createRef(),
-    React.createRef(),
-  ]);
+  const selectedVideosCount = videoSelections.filter(Boolean).length;
+  const videoSources = [
+    `${process.env.PUBLIC_URL}/vid1.mp4`,
+    `${process.env.PUBLIC_URL}/vid2.mp4`,
+    `${process.env.PUBLIC_URL}/test_4.mp4`,
+    `${process.env.PUBLIC_URL}/vid3.mp4`
+  ];
 
   return (
     <>
-      <div className='box'>
-        {!showVideo && (
-          <img className='img' src={img} alt="ok" />       
+      <div className={`box video-${selectedVideosCount}`}>
+        {selectedVideosCount === 0 && (
+          <img className='img' src={img} alt="No Camera Available" />       
         )}
       
-
-        {/* Existing JSX */}
-        {showVideo && (
-        <div className='video1'>
-          <video ref={videoRefs.current[0]}  autoPlay muted loop >
-            <source src={`${process.env.PUBLIC_URL}/vid1.mp4`} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video> 
-        </div>
-        )}
-
-        {showVideo && (
-        <div className='video2'>
-          <video ref={videoRefs.current[1]} autoPlay muted loop>
-            <source src={`${process.env.PUBLIC_URL}/vid2.mp4`} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-        )}
-
-      {showVideo && (
-        <div className='video3'>
-          <video ref={videoRefs.current[2]} autoPlay muted loop >
-            <source src={`${process.env.PUBLIC_URL}/test_4.mp4`} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-        )}
-
-        {showVideo && (
-        <div className='video4'>
-          <video ref={videoRefs.current[3]} autoPlay muted loop >
-            <source src={`${process.env.PUBLIC_URL}/vid3.mp4`} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-        )}      
+        {videoSelections.map((isSelected, index) => isSelected && (
+          <div key={index} className={`video video-${selectedVideosCount}`}>
+            <video autoPlay muted loop>
+              <source src={videoSources[index]} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video> 
+          </div>
+        ))}
       </div>
     </>
-  )
+  );
 }
 
-export default VideoPlayerTest
+export default VideoPlayerTest;
